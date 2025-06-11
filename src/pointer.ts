7c723c83cf1c860a710f25efc4a76e4a2666e991
James Prevett <jp@jamespre.dev>
2025-06-11 15:09:31 -0500

    Made `Pointer` much easier to modify and use

--- src/pointer.ts
+++ src/pointer.ts
@@ -13,7 +13,7 @@ export interface PointerJSON {
  * A pointer
  */
 @registerType
-export class Pointer<const T extends Type = Type> extends Number {
+export class Pointer<const T extends Type = Type> {
 	static size = 4;
 
 	static get(this: void, buffer: ArrayBufferLike, offset: number): Pointer<any> {
@@ -24,43 +24,55 @@ export class Pointer<const T extends Type = Type> extends Number {
 		__view(buffer).setUint32(offset, value.valueOf(), true);
 	}
 
+	#value: number;
+
 	public constructor(
 		public type: T,
-		address: number,
+		value: number,
 		public readonly memory: Memory<ArrayBufferLike> = _lastMemory
 			?? _throw(new SyntaxError('Missing required memory argument'))
 	) {
-		super(address);
+		this.#value = value;
 	}
 
 	public toString(): string {
-		return '0x' + this.valueOf().toString(16).padStart(8, '0');
+		return '0x' + this.#value.toString(16).padStart(8, '0');
 	}
 
 	public toJSON(): PointerJSON {
 		return {
 			typename: this.type.name,
-			address: this.valueOf(),
+			address: this.#value,
 		};
+	}
+
+	public valueOf(): number {
+		return this.#value;
+	}
+
+	public [Symbol.toPrimitive](): number {
+		return this.#value;
+	}
+
+	public set(newAddress: number | Pointer<any>): this {
+		this.#value = newAddress.valueOf();
+		return this;
+	}
+
+	public add(amount: number): this {
+		this.#value += amount * this.type.size;
+		return this;
 	}
 
 	/**
 	 * Cast the pointer to a different type.
 	 */
 	public as<C extends Type>(type: C): Pointer<C> {
-		return new Pointer(type, this.valueOf(), this.memory);
+		return new Pointer(type, this.#value, this.memory);
 	}
 
 	public deref(): Value<T> {
-		const mem = this.memory.at(this.valueOf());
+		const mem = this.memory.at(this.#value);
 		return this.type.get(mem.buffer, mem.byteOffset);
-	}
-
-	public increment(amount: number = 1): Pointer<T> {
-		return new Pointer(this.type, this.valueOf() + amount * this.type.size, this.memory);
-	}
-
-	public decrement(amount: number = 1): Pointer<T> {
-		return new Pointer(this.type, this.valueOf() - amount * this.type.size, this.memory);
 	}
 }
